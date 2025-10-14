ff6b02186c607e4557b9547cbd31382a0a4900ac
Kamil Kijak <kamil.kijak@pixlesofte.com>
2025-10-14 16:21:32 +0200

    created upploads for files required for app

--- utils/multerUploads.js
+++ utils/multerUploads.js
@@ -2,10 +2,15 @@
 const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
+const nanoID = require("nanoid");
+
+
+const IMAGE_MAX_MB_SIZE = 8;
+const FILE_MAX_MB_SIZE = 500;
 
 
 // MIME Filter
-const fileFilter = function (req, file, cb) {
+const imageFileFilter = function (req, file, cb) {
   if (file.mimetype.startsWith('image/')) {
     cb(null, true);
   } else {
@@ -18,9 +23,13 @@ const profileImageStorage = multer.diskStorage({
     destination:(req, file, cb) => {
         const folderPath = path.join(process.cwd(), `files`, `${req.session.userID}`);
         if(!fs.existsSync(folderPath)) {
-            fs.mkdirSync(folderPath, { recursive: true }, (err) => {
+            try {
+                fs.mkdirSync(folderPath, { recursive: true });
+                cb(null, folderPath);
+            } catch (err) {
                 console.log("Error with creating new folder: ", err);
-            });
+                cb(err, null);
+            }
         }
         cb(null, folderPath);
     },
@@ -29,13 +38,95 @@ const profileImageStorage = multer.diskStorage({
     }
 });
 
+const appImageStorage = multer.diskStorage({
+    destination:(req, file, cb) => {
+        // require req.body.ID_application
+        if(!req.body.ID_application) {
+            cb(new Error("ID_application is not defined"), folderPath);
+        }
+        const folderPath = path.join(process.cwd(), `files`, `${req.session.userID}`, "apps", `${req.body.ID_application}`);
+        if(!fs.existsSync(folderPath)) {
+            try {
+                fs.mkdirSync(folderPath, { recursive: true });
+                cb(null, folderPath);
+            } catch (err) {
+                console.log("Error with creating new folder: ", err);
+                cb(err, null);
+            }
+        }
+        cb(null, folderPath);
+    },
+    filename:(req, file, cb) => {
+        cb(null, `app.${path.extname(file.originalname)}`);
+    }
+});
+
+const appFileStorage = multer.diskStorage({
+    destination:(req, file, cb) => {
+        // require req.body.ID_application
+        if(!req.body.ID_application) {
+            cb(new Error("ID_application is not defined"), folderPath);
+        }
+        const folderPath = path.join(process.cwd(), `files`, `${req.session.userID}`, "apps", `${req.body.ID_application}`);
+        if(!fs.existsSync(folderPath)) {
+            try {
+                fs.mkdirSync(folderPath, { recursive: true });
+                cb(null, folderPath);
+            } catch (err) {
+                console.log("Error with creating new folder: ", err);
+                cb(err, null);
+            }
+        }
+        cb(null, folderPath);
+    },
+    filename:(req, file, cb) => {
+        cb(null, file.originalname);
+    }
+});
+
+const appScreensStorage = multer.diskStorage({
+    destination:(req, file, cb) => {
+        // require req.body.ID_application
+        if(!req.body.ID_application) {
+            cb(new Error("ID_application is not defined"), folderPath);
+        }
+        const folderPath = path.join(process.cwd(), `files`, `${req.session.userID}`, "apps", `${req.body.ID_application}`, "screens");
+        if(!fs.existsSync(folderPath)) {
+            try {
+                fs.mkdirSync(folderPath, { recursive: true });
+                cb(null, folderPath);
+            } catch (err) {
+                console.log("Error with creating new folder: ", err);
+                cb(err, null);
+            }
+        }
+        cb(null, folderPath);
+    },
+    filename:(req, file, cb) => {
+        cb(null, `${nanoID.nanoid()}.${path.extname(file.originalname)}`);
+    }
+});
+
+const appFileUpload = multer({
+    storage:appFileStorage,
+    limits: { fileSize: FILE_MAX_MB_SIZE * 1024 * 1024 }
+});
+
+const appImageUpload = multer({
+    storage:appImageStorage,
+    fileFilter:imageFileFilter,
+    limits: { fileSize: IMAGE_MAX_MB_SIZE * 1024 * 1024 }
+})
+
 const profileImageUpload = multer({
     storage:profileImageStorage,
-    fileFilter:fileFilter,
+    fileFilter:imageFileFilter,
     // max 8 MB
-    limits: { fileSize: 8 * 1024 * 1024 }
+    limits: { fileSize: IMAGE_MAX_MB_SIZE * 1024 * 1024 }
 })
 
 module.exports = {
-    profileImageUpload
+    profileImageUpload,
+    appImageUpload,
+    appFileUpload
 }
