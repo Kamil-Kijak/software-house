818dda1135f8dc863915c0e85157b9cfe91eed44
Kamil Kijak <kamil.kijak@pixlesofte.com>
2025-10-21 15:30:41 +0200

    added cors

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ require("dotenv").config();
 
 const path = require("path");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 const express = require("express");
 const http = require("http");
@@ -24,6 +25,13 @@ const appTagRouter = require("./routes/appTags");
 const appScreensRouter = require("./routes/appScreens");
 
 const app = express();
+
+
+app.use(cors({
+    origin:"http://localhost:3000",
+    methods:["GET", "POST", "PUT", "DELETE"],
+    credentials:true
+}))
 
 app.use(express.json());
 app.use(cookieParser());
