32399c1a1128a7ddd761377139e49e4b8ff507d2
Kamil Kijak <kamil.kijak@pixlesofte.com>
2025-10-30 23:54:38 +0100

    upgraded socialLinks endpoints

--- routes/socialLinks.js
+++ routes/socialLinks.js
@@ -10,6 +10,8 @@ const sqlQuery = require("../utils/mysqlQuery");
 const authorization = require("../utils/authorization");
 const { body, validationResult } = require("express-validator");
 
+const SocialLink = require("../models/SocialLink");
+
 const router = express.Router();
 
 // Constants
@@ -26,26 +28,31 @@ router.use(authorization());
 
 // request social links of session user
 router.get("/my_links", async (req, res) => {
-    const socialLinksResult = await sqlQuery(res, "SELECT ID, name, href FROM social_links WHERE ID_user = ?", [req.session.userID]);
-    res.status(200).json({message:"Retrivied socials links", socialLinks:socialLinksResult});
+    const socialLinks = await SocialLink.findAll({attributes:["id", "name", "href"], where:{idUser:req.session.userID}})
+    res.status(200).json({message:"Retrivied socials links", socialLinks});
 });
 
 
 // adding new link to session user
-router.post("/insert_link", [checkBody(["name", "href"]),
-    body("name").trim().isLength({max:25}).withMessage("Is too long"),
-    body("href").trim().isLength({max:255}).withMessage("Is too long")
+router.post("/insert_link", [
+    body("name").trim().exists({checkFalsy:true}).withMessage("name is required").isLength({max:25}).withMessage("name max length: 25"),
+    body("href").trim().exists({checkFalsy:true}).withMessage("href is required").isLength({max:255}).withMessage("href max length: 255")
 ], async (req, res) => {
-    if(!validationResult(req).isEmpty()) {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
         return res.status(400).json({errors: errors.array()});
     }
     const {name, href} = req.body;
     const trimmedName = name.trim();
     const trimmedHref = href.trim();
-    const socialLinkCountResult = await sqlQuery(res, "SELECT COUNT(ID) as count FROM social_links WHERE ID_user = ?", [req.session.userID]);
+    const socialLinksCount = await SocialLink.count({where:{idUser:req.session.userID}});
     // checking user's social links count limit
-    if(socialLinkCountResult[0].count <= MAX_LINKS_PER_USER) {
-        await sqlQuery(res, "INSERT INTO social_links() VALUES(?, ?, ?, ?)", [nanoID.nanoid(), trimmedName, trimmedHref, req.session.userID]);
+    if(socialLinksCount <= MAX_LINKS_PER_USER) {
+        await SocialLink.create({
+            name:trimmedName,
+            href:trimmedHref,
+            idUser:req.session.userID
+        });
         res.status(201).json({message:"Inserted successfully"});
     } else {
         res.status(400).json({error:"Too many social links for this user"});
@@ -53,32 +60,36 @@ router.post("/insert_link", [checkBody(["name", "href"]),
 });
 
 // update session user link by link ID
-router.put("/update_link", [checkBody(["IDLink", "name", "href"]),
-    body("name").trim().isLength({max:25}).withMessage("Is too long"),
-    body("href").trim().isLength({max:255}).withMessage("Is too long")
+router.put("/update_link", [
+    body("idLink").exists({checkFalsy:true}).withMessage("idLink is required"),
+    body("name").trim().exists({checkFalsy:true}).withMessage("name is required").isLength({max:25}).withMessage("name max length: 25"),
+    body("href").trim().exists({checkFalsy:true}).withMessage("href is required").isLength({max:255}).withMessage("href max length: 255")
 ], async (req, res) => {
-    if(!validationResult(req).isEmpty()) {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
         return res.status(400).json({errors: errors.array()});
     }
-    const {IDLink, name, href} = req.body;
+    const {idLink, name, href} = req.body;
     const trimmedName = name.trim();
     const trimmedHref = href.trim();
-    const linkOwnershipResult = await sqlQuery(res, "SELECT COUNT(ID) as count FROM social_links WHERE ID_user = ? AND ID = ?", [req.session.userID, IDLink]);
-    if(linkOwnershipResult[0].count >= 1) {
-        const updateResult = await sqlQuery(res, "UPDATE social_links SET name = ?, href = ? WHERE ID = ?", [trimmedName, trimmedHref, IDLink]);
-        res.status(200).json({message:"Update succeed", updated:updateResult.affectedRows});
+    const linkOwnership = await SocialLink.count({where:{idUser:req.session.userID, id:idLink}});
+    if(linkOwnership >= 1) {
+        const [affectedRows] = await SocialLink.update({name:trimmedName, href:trimmedHref}, {where:{id:idLink}})
+        res.status(200).json({message:"Update succeed", affectedRows});
     } else {
         res.status(403).json({error:"You don't have permission for update this resource"});
     }
 });
 
 // delete session user link
-router.delete("/delete_link", checkBody(["IDLink"]), async (req, res) => {
-    const {IDLink} = req.body;
-    const linkOwnershipResult = await sqlQuery(res, "SELECT COUNT(ID) as count FROM social_links WHERE ID_user = ? AND ID = ?", [req.session.userID, IDLink]);
-    if(linkOwnershipResult[0].count >= 1) {
-        const deleteResult = await sqlQuery(res, "DELETE FROM social_links WHERE ID = ?", [IDLink]);
-        res.status(200).json({message:"Delete succeed", deleted:deleteResult.affectedRows});
+router.delete("/delete_link", [
+    body("idLink").exists({checkFalsy:true}).withMessage("idLink is required")
+], async (req, res) => {
+    const {idLink} = req.body;
+    const linkOwnership = await SocialLink.count({where:{idUser:req.session.userID, id:idLink}});
+    if(linkOwnership >= 1) {
+        const affectedRows = await SocialLink.destroy({where:{id:idLink}});
+        res.status(200).json({message:"Delete succeed", affectedRows});
     } else {
         res.status(403).json({error:"You don't have permission for delete this resource"});
     }
